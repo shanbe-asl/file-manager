2be2b8d366c3611539ef653e58fe8667e5fa1a5d
shanbe-asl <qadamshoev.s@gmail.com>
2024-02-06 04:45:58 +0600

    feat: implement hash

---
+++ hash.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import crypto from 'crypto';
+
+function hash(pathToFile) {
+    const hash = crypto.createHash('sha256');
+    const stream = fs.createReadStream(pathToFile);
+    
+    stream.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+
+    stream.on('end', () => {
+        const fHash = hash.digest('hex');
+        console.log(`'${pathToFile}' hash: ${fHash}`);
+    });
+
+    stream.on('error', (error) => {
+        console.error(`Error reading the file: ${error.message}`);
+    });
+}
+
+export{hash};
