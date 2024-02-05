b781f818b34e92cf57110489f7cf88293025f169
shanbe-asl <qadamshoev.s@gmail.com>
2024-02-06 04:45:28 +0600

    feat: implement os info

---
+++ os.js
@@ -0,0 +1,30 @@
+import exp from "constants";
+import os from "os";
+
+function EOL() {
+  console.log(`EOL: '${os.EOL}'`);
+}
+
+function cpus() {
+  const cpus = os.cpus();
+  console.log("CPU Information: ");
+  cpus.forEach((cpu, index) => {
+    console.log(`CPU ${index + 1}:`);
+    console.log(`Model: ${cpu.model}`);
+    console.log(`Clock Rate: ${cpu.speed / 1000} GHz`);
+  });
+}
+
+function homedir() {
+  console.log(`Home Directory: ${os.homedir()}`);
+}
+
+function sysUsername() {
+  console.log(`System Username: ${os.userInfo().username}`);
+}
+
+function architecture() {
+  console.log(`CPU Architecture: ${os.arch()}`);
+}
+
+export { EOL, cpus, homedir, sysUsername, architecture };
